10e82ec672c87704561fbed14625141239a8a7bc
sy0201 <noey0201@gmail.com>
2024-11-26 11:15:12 +0900

    [INIT] : 프로젝트 초기화

---
+++ KoKKiosk_SYver/KoKKiosk_SYver/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  KoKKiosk_SYver
+//
+//  Created by siyeon park on 11/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
